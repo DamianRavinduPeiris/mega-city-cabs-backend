ad7dad2c19e8cdce9f66657081b9dbb90dccb68f
DamianRavinduPeiris <drpeiris3@gmail.com>
2025-03-16 00:45:21 +0530

    Added a health check endpoint.

---
+++ src/main/java/com/damian/megacity/controller/HealthCheckController.java
@@ -0,0 +1,67 @@
+package com.damian.megacity.controller;
+
+import com.damian.megacity.response.Response;
+import com.google.gson.Gson;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static com.damian.megacity.service.constants.HealthConstants.*;
+
+@WebServlet(name = HEALTH_CONTROLLER, urlPatterns = HEALTH_ENDPOINT)
+@Log
+public class HealthCheckController extends HttpServlet {
+    private static final String HEALTH_CHECK_URL = System.getenv(INVOKING_URL);
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final Gson gson = new Gson();
+
+    @Override
+    public void init() {
+        scheduler.scheduleAtFixedRate(this::invokeHealthCheck, 0, 3, TimeUnit.MINUTES);
+    }
+
+    private void invokeHealthCheck() {
+        try {
+            var httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(HEALTH_CHECK_URL))
+                    .build();
+
+            var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            log.info("Health Check Response: " + response.body());
+        } catch (Exception e) {
+            log.severe("Health check failed: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws IOException {
+        servletResponse
+                .getWriter()
+                .println(gson.toJson(createAndBuildResponse(200, HEALTH_STATUS_UP, null)));
+    }
+
+    @Override
+    public void destroy() {
+        scheduler.shutdown();
+    }
+
+    public Response createAndBuildResponse(int status, String msg, Object data) {
+        return Response.builder()
+                .status(status)
+                .message(msg)
+                .data(data)
+                .build();
+    }
+}
---
+++ src/main/java/com/damian/megacity/service/constants/HealthConstants.java
@@ -0,0 +1,12 @@
+package com.damian.megacity.service.constants;
+
+public class HealthConstants {
+    public static final String HEALTH_ENDPOINT = "/api/v1/health";
+    public static final String HEALTH_CONTROLLER = "healthController";
+    public static final String HEALTH_STATUS_UP = "UP!";
+    public static final String HEALTH_STATUS_DOWN = "DOWN!";
+    public static final String INVOKING_URL = "INVOKING_URL";
+
+    private HealthConstants() {
+    }
+}
